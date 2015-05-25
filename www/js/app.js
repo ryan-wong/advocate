4000330726606c4ad6dd7bbdb5d23af39e67c060
ryanwong <wongryan2001@gmail.com>
2015-05-25 18:01:18 -0400

    finish app

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('advocate', ['ionic'])
+angular.module('advocate', ['ionic', 'ngMessages', "base64"])
 
-.run(function($ionicPlatform, $rootScope) {
+.run(function($ionicPlatform, $rootScope, storageService) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,8 +16,16 @@ angular.module('advocate', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+  storageService.fillGaps();
   $rootScope.data = data;
-  $rootScope.category = [];
+  $rootScope.count = storageService.getField('count',"");
+  $rootScope.username = storageService.getField('name',"");
+  $rootScope.educationLevel = storageService.getField('education',"");
+  $rootScope.age = false;
+  $rootScope.disability = false;
+  $rootScope.email  = "";
+  $rootScope.researchEmail = data.researcher_email;
+  $rootScope.plans = [];
 })
 .config(function($provide) {
     $provide.decorator('$state', function($delegate, $stateParams) {
@@ -90,65 +98,316 @@ angular.module('advocate', ['ionic'])
   }];
 
 })
-.controller('WelcomeCtrl', ['$scope', WelcomeCtrl])
-.controller('QuestionCtrl', ['$scope', '$rootScope', '$state', QuestionCtrl])
-.controller('ResultCtrl', ['$scope', ResultCtrl])
-.controller('SpecificQuestionCtrl', ['$scope',  '$rootScope', '$state', SpecificQuestionCtrl])
-.controller('EmailContentCtrl', ['$scope', EmailContentCtrl])
+.controller('WelcomeCtrl', ['$scope', 'storageService', '$state',  WelcomeCtrl])
+.controller('EducationCtrl', ['$scope','$rootScope', 'storageService', '$state',  EducationCtrl])
+.controller('AgeCtrl', ['$scope', '$rootScope', '$state', AgeCtrl])
+.controller('DisabilityCtrl', ['$scope', '$rootScope', '$state', DisabilityCtrl])
+.controller('QuestionCtrl', ['$scope', '$rootScope', '$state', '$ionicPopup', QuestionCtrl])
+.controller('ResultCtrl', ['$scope', '$rootScope', '$state', 'emailService', 'storageService', ResultCtrl])
+.controller('EmailContentCtrl', ['$scope', '$ionicPopup', '$rootScope', 'storageService', 'emailService', EmailContentCtrl])
+.service('storageService', [storageService])
+.service('emailService', ["$http", "$base64",emailService])
 
-function WelcomeCtrl($s){
-}
-
-function SpecificQuestionCtrl($s, $r, $st){
-console.log($r.category);
-$s.plans = [];
-$r.category.map(function(oneCategory){
-  var categoryFound = $r.data.categories.filter(function(onePlan){
-  return onePlan.id == oneCategory;
-});
-  console.log('cat', categoryFound);
-  if (categoryFound.length > 0){
-    for (var i = 0; i < categoryFound[0]['plans'].length; i++) {
-      if ($s.plans.indexOf(categoryFound[0]['plans'][i]) < 0){
-        $s.plans.push(categoryFound[0]['plans'][i]);
-      }
-    };
-  }
-});
-console.log($s.plans);
-}
-
-function QuestionCtrl($s, $r, $st){
-$s.category = [
-false,
-false,
-false,
-false,
-false,
-false,
-false
-];
-$s.save = function(i){
-  $s.category[i-1] = !$s.category[i-1];
-};
-$s.continue = function(){
-  var hasOneCategory = false;
-  for (var i = 0; i < $s.category.length; i++){
-    if ($s.category[i]){
-      $r.category.push(i+1);
-      hasOneCategory = true;
+function WelcomeCtrl($s, sS, $st){
+  var hasName = sS.getField('name',"");
+  var hasEducation = sS.getField('education',"");
+  $s.whichPage = function(){
+    if (hasName.length > 0 && hasEducation.length > 0){
+      $st.transitionTo('age', null,{refresh:true, inherit:true, notify:true});
+    }else{
+      $st.transitionTo('education', null,{refresh:true, inherit:true, notify:true});
     }
   }
-  if (hasOneCategory){
-    $st.transitionTo('specificQuestion',null, null, null);
+}
+
+function AgeCtrl($s, $r, $st){
+  $s.yesno = function(answer){
+    if (answer == 1){
+      $r.age = true;
+      $r.plans = [7];
+      $st.transitionTo('disability', null,{refresh:true, inherit:true, notify:true});
+    }else{
+      $r.age = false;
+      $r.plans = [];
+      $st.transitionTo('question', null,{refresh:true, inherit:true, notify:true});
+    }
+  }
+}
+function DisabilityCtrl($s, $r, $st){
+  $s.yesno = function(answer){
+    if (answer == 1){
+      $r.disability = true;
+      $r.plans = [7,2];
+      $st.transitionTo('result', null,{refresh:true, inherit:true, notify:true});
+    }else{
+      $r.disability = false;
+      $r.plans = [7];
+      $st.transitionTo('result', null,{refresh:true, inherit:true, notify:true});
+    }
+  }
+}
+function QuestionCtrl($s, $r, $st, $i){
+$s.questions = $r.data.benefitQuestion.filter(function(oneQuestion){
+  return oneQuestion.show;
+});
+$s.continue = function(){
+  var hasChecked = false;
+  $s.questions.map(function(oneQuestion){
+    if (oneQuestion.checked){
+      hasChecked = true;
+      $r.plans.push(oneQuestion.id);
+    }
+  });
+  if (hasChecked){
+    $st.transitionTo('result', null,{refresh:true, inherit:true, notify:true});
+  } else{
+     $i.alert({
+        title: 'Missing Answers',
+        template: "Please check off which one that applies to you?",
+        buttons: [
+        { text: 'Ok',
+          type: 'button-royal'
+        }
+        ]
+      });
   }
 };
 }
 
-function ResultCtrl($s){
-
+function ResultCtrl($s, $r, $st, eS, sS){
+  $s.showPlans = $r.data.plans.filter(function(onePlan){
+    var or = onePlan.or;
+    var and = onePlan.and;
+    var not = (onePlan.not != undefined)?onePlan.not:[];
+    if (not.length > 0){
+      var match = true;
+      for (var i = 0; i < or.length; i++){
+        if ($r.plans.indexOf(or[i]) == -1){
+          match = false;
+        }
+      }
+      return match;
+    }
+    if (and.length > 0){
+      var match = true;
+      for (var i = 0; i < and.length; i++){
+        if ($r.plans.indexOf(and[i]) > -1){
+          match = match && true;
+        } else{
+          match = match && false;
+        }
+      }
+      return match;
+    //if ends
+    }
+    if (or.length > 0){
+      var match = false;
+      for (var i = 0; i < or.length; i++){
+        if ($r.plans.indexOf(or[i]) > -1){
+          match = true;
+        }
+      }
+      return match;
+    //if ends
+    }
+    return true;
+  });
+  $s.toggleGroup = function(group) {
+    if ($s.isGroupShown(group)) {
+      $s.shownGroup = null;
+    } else {
+      $s.shownGroup = group;
+    }
+  };
+  $s.isGroupShown = function(group) {
+    return $s.shownGroup === group;
+  };
+  $r.count++;
+  sS.setField('count', 'integer', $r.count);
+  var content = "<p>Name: " + $r.username + "<br/>" +
+                "<p>Education: " + $r.educationLevel + "<br/>" +
+                "<p>Count: " + $r.count + "<br/>" +
+                "<p>Age > 65: " + (($r.age)?"yes":"no") + "<br/>" +
+                "<p>Disability: " + (($r.disability)?"yes":"no") + "<br/>" +
+                "<p>Other: " + $r.plans.join(";") + "<br/>" ;
+  var csv = $r.username + "," + $r.educationLevel + "," + $r.count + "," + (($r.age)?"yes":"no") + "," + (($r.disability)?"yes":"no") + ","  + $r.plans.join(";");
+  eS.sendEmail($r.data.researcher_email, "Research Report", "Research Report", content, csv, function(e, data){
+    console.log('e', e);
+    console.log('data', data);
+  });
+  sS.setField("planHolder", "object", $s.showPlans);
 }
 
-function EmailContentCtrl($s){
+function EmailContentCtrl($s, $i, $r, sS, eS ){
+$s.profile = {
+  email: ""
+};
 
+$s.send = function(form){
+  if(form.$valid) {
+    var plans = sS.getField('planHolder',[]);
+    var content = "";
+    plans.map(function(onePlan){
+      content = content + "<h1>" + onePlan.text + "</h1>" + onePlan.description;
+      return onePlan;
+    });
+    content = content + $r.data.printout;
+  eS.sendEmail($s.profile.email, $r.username, "Advocate App Benefit Programs", content, "", function(e, data){
+    console.log('e', e);
+    console.log('data', data);
+     $i.alert({
+        title: 'Email Sent',
+        template: "We just sent an email out to you",
+        buttons: [
+        { text: 'Ok',
+          type: 'button-royal'
+        }
+        ]
+      });
+  });
+  }
+}
+}
+
+function EducationCtrl($s, $r, sS, $st){
+$s.options = [
+'Public School',
+'High School',
+'College',
+'University',
+'Graduate School'
+];
+$s.profile = {
+  name: "",
+  education:"Public School"
+};
+
+$s.signUp = function(form){
+  if(form.$valid) {
+    sS.setField('name', 'string', $s.profile.name);
+    sS.setField('education', 'string', $s.profile.education);
+    $r.username = $s.profile.name;
+    $r.educationLevel = $s.profile.education;
+    $st.transitionTo('age', null,{refresh:true, inherit:true, notify:true});
+  }
+}
+}
+
+function storageService () {
+ var storageList = {
+  namespace: "advocate",
+  fields: {
+   name:{type: 'string', value:""},
+   education:{type: 'string', value:""},
+   count:{type: 'integer', value:0},
+   data: {type: 'object', value: ""},
+   planHolder: {type: 'object', value: ""}
+  }
+ };
+  var _keys = Object.keys(storageList.fields);
+  var namespace = storageList.namespace;
+  this.reset = function(){
+    for (var i = 0; i < _keys.length; i++){
+      window.localStorage.setItem(namespace + _keys[i] , JSON.stringify(storageList.fields[_keys[i]].value));
+    }
+  };
+  this.fillGaps = function(){
+    for (var i = 0; i < _keys.length; i++){
+      this.hasField(_keys[i]);
+    }
+  };
+  //retrieve field
+  this.getField = function(field, defaultValue){
+    //if not a valid field, just give back defaultValue
+    if (!this.hasField(field)){
+      return defaultValue;
+    }else{
+      //find field
+      var result = JSON.parse(localStorage.getItem(namespace + field));
+      //if invalid field reset it
+      if (result == null){
+        window.localStorage.setItem(namespace + field , JSON.stringify(storageList.fields[field].value));
+      }
+      if (storageList.fields[field].type == 'float'){
+        result = Number(result);
+      }
+      if (storageList.fields[field].type == 'integer'){
+        result = Number(result);
+      }
+      return result;
+    }
+  };
+
+  this.setField = function(field, type, value){
+    var result = value;
+    if (type =='float'){
+      result = Number(result);
+    }else if (type =='integer'){
+      result = Number(result);
+    }
+      window.localStorage.setItem(namespace + field, JSON.stringify(result));
+      return result;
+  };
+  //check if field exist in local storage
+  this.hasField = function(field){
+    //if field is not registered field return false
+    if (_keys.indexOf(field) < 0){
+      return false;
+    }
+    //find field
+    var result = JSON.parse(localStorage.getItem(namespace + field));
+    //if not there, create it to default
+    if (result == null){
+      window.localStorage.setItem(namespace + field , JSON.stringify(storageList.fields[field].value));
+      return false;
+    }else{
+      return true;
+    }
+  };
+  //reset specific field
+  this.clearField = function(field){
+    if (this.hasField(field)){
+     window.localStorage.setItem(namespace + field , JSON.stringify(storageList.fields[field].value));
+    }
+  };
+  return this;
+}
+
+function emailService($http, $base64){
+  this.sendEmail = function(email, name, subject, content, attachment, cb){
+    var data = {
+        "key" : "OdzE9_MvlC2bOlhUyApG7g",
+        "message": {
+          "from_email" : "sameer@sixtooth.com",
+          "to" : [{
+            "email" : email,
+            "name" : name,
+            "type" : 'to'
+          }],
+          "subject" : subject,
+          "html" : content
+        }
+      };
+    if (attachment.length > 0){
+      data["message"]["attachments"] = [
+        {
+          "type": "text/plain",
+          "name": "research.csv",
+          "content": $base64.encode(attachment)
+        }
+      ];
+    }
+  $http({
+      method: 'POST',
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: data
+    })
+    .success(function(data) {
+      cb(null, data);
+    })
+    .error(function(err,status) {
+      cb(err);
+    });
+  }
+  return this;
 }
--- www/js/config.js
+++ www/js/config.js
@@ -2,34 +2,34 @@ var welcome = {
   name: 'welcome',
   url: '/welcome',
   controller: 'WelcomeCtrl',
-  secure: true,
-  cache: false,
   templateUrl: 'templates/welcome.html'
+};
+
+var age = {
+  name: 'age',
+  url: '/age',
+  controller: 'AgeCtrl',
+  templateUrl: 'templates/age.html'
+};
+
+var disability = {
+  name: 'disability',
+  url: '/disability',
+  controller: 'DisabilityCtrl',
+  templateUrl: 'templates/disability.html'
 };
 
 var question = {
   name: 'question',
   url: '/question',
   controller: 'QuestionCtrl',
-  secure: true,
-  cache: false,
   templateUrl: 'templates/question.html'
-};
-
-var specificQuestion = {
-  name: 'specificQuestion',
-  url: '/specificQuestion',
-  controller: 'SpecificQuestionCtrl',
-  secure: true,
-  cache: false,
-  templateUrl: 'templates/specificQuestion.html'
 };
 
 var result = {
   name: 'result',
   url: '/result',
   controller: 'ResultCtrl',
-  secure: true,
   cache: false,
   templateUrl: 'templates/result.html'
 };
@@ -38,14 +38,21 @@ var emailContent = {
   name: 'emailContent',
   url: '/emailContent',
   controller: 'EmailContentCtrl',
-  secure: true,
-  cache: false,
   templateUrl: 'templates/emailContent.html'
+};
+
+var education = {
+  name: 'education',
+  url: '/education',
+  controller: 'EducationCtrl',
+  templateUrl: 'templates/education.html'
 };
 var stateList = [
 welcome,
+age,
+disability,
 question,
 result,
 emailContent,
-specificQuestion
+education
 ];
